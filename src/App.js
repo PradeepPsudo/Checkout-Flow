8d6bbc2d41d670e5f882a81f991b49ea61192ef9
Pradeep P <pradeepp1994@gmail.com>
2024-09-03 15:54:24 +0530

    Integrating API

--- src/App.js
+++ src/App.js
@@ -7,28 +7,17 @@ import { useGetProductList } from './common/customhooks/useGetProducts';
 import { API_ENDPOINTS } from './constants/apiConstants';
 import ClipLoader from 'react-spinners/ClipLoader'; 
 import StepFlow from './components/Step';
-// import ErrorBoundary from './components/ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary';
 
-import { ErrorBoundary } from 'react-error-boundary'
 
-function MyFallbackComponent({ error, resetErrorBoundary }) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  )
-}
+
 function App() {
   let classifiedProductsList ={};
   const {data,isLoading} = useGetProductList(API_ENDPOINTS.GET_PRODUCTS);
   if(data.products){
     classifiedProductsList = getClassifiedProducts(data);
   }
-  // if(error){
-  //   throw error;
-  // }
+ 
 
   if (isLoading) {
     return (<div className="spinner">
@@ -36,14 +25,8 @@ function App() {
       </div>)
   }
   return (
-    // <ErrorBoundary>
-    <ErrorBoundary
-      FallbackComponent={MyFallbackComponent}
-      onReset={() => {
-        // reset the state of your app here
-      }}
-      resetKeys={['someKey']}
-    >
+    <ErrorBoundary>
+    
            <ProductsCheckoutContextProvider>
       <Header/>
       <div className="App">
@@ -51,8 +34,8 @@ function App() {
     </div>
     <Footer/>
     </ProductsCheckoutContextProvider>
-    </ErrorBoundary>
-    // </ErrorBoundary>
+     </ErrorBoundary>
+ 
      
     
     
